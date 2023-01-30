525be6a2ded18959cbcb424dbf55042eaba80bc2
David Rauh <drauh@ipb-halle.de>
2023-01-30 14:00:26 +0100

    Changed generate script to download generator

--- scripts/generate_api_server.sh
+++ scripts/generate_api_server.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-
-OPENAPI_GENERATOR=~/bin/openapi-generator-cli.sh
+OPENAPI_GENERATOR=bin/openapi-generator-cli.sh
 export GO_POST_PROCESS_FILE="gofmt -w"
 
 MB3_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 
 cd ${MB3_DIR}/scripts
+mkdir bin
+curl https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh > bin/openapi-generator-cli.sh
+chmod +x bin/openapi-generator-cli.sh
 ${OPENAPI_GENERATOR}  generate -c ${MB3_DIR}/scripts/config-openapi.yaml -o ${MB3_DIR}/cmd/mb3server/
