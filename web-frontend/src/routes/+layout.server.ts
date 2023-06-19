23f9ecdad2fa914a19ae9a37862b611d6c717918
David Rauh <drauh@ipb-halle.de>
2023-06-19 13:13:28 +0200

    Add configuration for deploy containers

--- web-frontend/src/routes/+layout.server.ts
+++ web-frontend/src/routes/+layout.server.ts
@@ -1,6 +1,6 @@
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
     return {
-        baseurl: import.meta.env.VITE_SERVER_API_URL,
+        baseurl: typeof import.meta.env.VITE_SERVER_API_URL !== 'undefined' && import.meta.env.VITE_SERVER_API_URL !== null && import.meta.env.VITE_SERVER_API_URL !== "" ? import.meta.env.VITE_SERVER_API_URL : process.env.MB3_API_URL,
     };
 }
