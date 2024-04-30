899c281fd71f74d714ac2def2698f1ca77fdc3ad
Rene Meier <meier.rene@googlemail.com>
2024-04-30 15:24:16 +0200

    Take over code from https://github.com/cthum/massbank3-similarity-plugin.

--- api-similarity-score/similarity-score-impl/__main__.py
+++ api-similarity-score/similarity-score-impl/__main__.py
@@ -3,9 +3,14 @@
 import connexion
 
 from openapi_server import encoder
+import db
 
 
 def main():
+    print("Loading spectra... ", end='', flush=True)
+    db.load_spectra()
+    print("Finished")
+
     app = connexion.App(__name__, specification_dir='..')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
--- api-similarity-score/similarity-score-impl/similarity-score-controller.py
+++ api-similarity-score/similarity-score-impl/similarity-score-controller.py
@@ -1,11 +1,12 @@
 import connexion
-from typing import Dict
-from typing import Tuple
-from typing import Union
+import numpy as np
+from matchms import Spectrum, calculate_scores
+from matchms.similarity import CosineGreedy
 
+from db import spectra
+from openapi_server.models import SimilarityScore
 from openapi_server.models.similarity_calculation import SimilarityCalculation  # noqa: E501
 from openapi_server.models.similarity_score_list import SimilarityScoreList  # noqa: E501
-from openapi_server import util
 
 
 def similarity_post(similarity_calculation):  # noqa: E501
@@ -19,8 +20,36 @@ def similarity_post(similarity_calculation):  # noqa: E501
     :rtype: Union[SimilarityScoreList, Tuple[SimilarityScoreList, int], Tuple[SimilarityScoreList, int, Dict[str, str]]
     """
     if connexion.request.is_json:
-        similarity_calculation = SimilarityCalculation.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
+        request = SimilarityCalculation.from_dict(similarity_calculation)
+
+        mz = []
+        intensities = []
+
+        for peak in request.peak_list:
+            mz.append(peak.mz)
+            intensities.append(peak.intensity)
+
+        query = Spectrum(mz=np.array(mz), intensities=np.array(intensities))
+
+        if len(request.reference_spectra_list) > 0:
+            def filter_fn(spectrum):
+                return spectrum.metadata['id'] in request.reference_spectra_list
+
+            references = list(filter(filter_fn, spectra))
+
+            scores = calculate_scores(references, [query], CosineGreedy())
+        else:
+            scores = calculate_scores(spectra, [query], CosineGreedy())
+        matches = scores.scores_by_query(query, 'CosineGreedy_score', sort=True)
+
+        match_list = []
+
+        for match in matches:
+            match_list.append(SimilarityScoreList(SimilarityScore(match[0].metadata['id'], match[1][0])))
+            #match_list.append(SimilarityScoreListInner(match[0].metadata['id'], match[1][0]))
+
+
+        return match_list
 
 
 def version_get():  # noqa: E501
