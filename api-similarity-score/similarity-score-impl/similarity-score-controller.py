93532705d2ad11a51c812810372966ad08eea4ef
Rene Meier <meier.rene@googlemail.com>
2024-04-30 11:34:57 +0200

    Workaround a issue in openapi-generator(https://github.com/OpenAPITools/openapi-generator/issues/1666)

--- api-similarity-score/similarity-score-impl/similarity-score-controller.py
+++ api-similarity-score/similarity-score-impl/similarity-score-controller.py
@@ -3,23 +3,23 @@ from typing import Dict
 from typing import Tuple
 from typing import Union
 
-from openapi_server.models.similarity_post_request import SimilarityPostRequest  # noqa: E501
-from openapi_server.models.similarity_score_list_inner import SimilarityScoreListInner  # noqa: E501
+from openapi_server.models.similarity_calculation import SimilarityCalculation  # noqa: E501
+from openapi_server.models.similarity_score_list import SimilarityScoreList  # noqa: E501
 from openapi_server import util
 
 
-def similarity_post(similarity_post_request):  # noqa: E501
+def similarity_post(similarity_calculation):  # noqa: E501
     """Create a new similarity calculation.
 
      # noqa: E501
 
-    :param similarity_post_request: a similarity job
-    :type similarity_post_request: dict | bytes
+    :param similarity_calculation: a similarity job
+    :type similarity_calculation: dict | bytes
 
-    :rtype: Union[List[SimilarityScoreListInner], Tuple[List[SimilarityScoreListInner], int], Tuple[List[SimilarityScoreListInner], int, Dict[str, str]]
+    :rtype: Union[SimilarityScoreList, Tuple[SimilarityScoreList, int], Tuple[SimilarityScoreList, int, Dict[str, str]]
     """
     if connexion.request.is_json:
-        similarity_post_request = SimilarityPostRequest.from_dict(connexion.request.get_json())  # noqa: E501
+        similarity_calculation = SimilarityCalculation.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
 
 
@@ -31,4 +31,4 @@ def version_get():  # noqa: E501
 
     :rtype: Union[str, Tuple[str, int], Tuple[str, int, Dict[str, str]]
     """
-    return 'do some magic magic!'
+    return 'cosine similarity 1.0.0'
