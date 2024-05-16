b72119b024129fe4e19bcb7e823d1468d38c731d
René Meier <meier.rene@googlemail.com>
2024-05-16 13:56:46 +0200

    Continue test infra

---
+++ similarity-service-cosine/test/test_db.py
@@ -0,0 +1,14 @@
+from pytest_postgresql import factories
+from pathlib import Path
+
+
+new_postgresql = factories.postgresql_proc()
+database = factories.postgresql("new_postgresql", dbname="massbank3", load=[Path("test_dump.sql")])
+
+
+def test_postgres_docker(database):
+    """Run test."""
+    cur = database.cursor()
+    #cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+    #database.commit()
+    cur.close()
