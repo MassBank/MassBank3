7f8c2ad86e314609fdc299e192b4461792757763
René Meier <meier.rene@googlemail.com>
2024-07-18 16:20:05 +0200

    Improve to connection pool

--- similarity-service-cosine/cosine_impl/__main__.py
+++ similarity-service-cosine/cosine_impl/__main__.py
@@ -4,6 +4,7 @@ import connexion
 
 from openapi_server import encoder
 
+
 def main():
     app = connexion.App(__name__, specification_dir='..')
     app.app.json_encoder = encoder.JSONEncoder
--- similarity-service-cosine/cosine_impl/cosine_impl_controller.py
+++ similarity-service-cosine/cosine_impl/cosine_impl_controller.py
@@ -1,24 +1,80 @@
+import logging
+import os
+from datetime import datetime
+
 import connexion
-import numpy as np
+import numpy
+import psycopg
+from psycopg_pool import ConnectionPool
 from flask import jsonify
-from matchms import Spectrum, calculate_scores
+from matchms import calculate_scores, set_matchms_logger_level, Spectrum
 from matchms.filtering import normalize_intensities
 from matchms.similarity import CosineGreedy
 
-from cosine_impl.db import ReferenceSpectra
 from openapi_server.models import SimilarityScore
 from openapi_server.models.similarity_calculation import SimilarityCalculation  # noqa: E501
 from openapi_server.models.similarity_score_list import SimilarityScoreList  # noqa: E501
 
-import os
-import psycopg
-
+# Environment variables for database connection
 DB_PORT = os.environ.get('DB_PORT', 5432)
 DB_USER = os.environ.get('DB_USER', "massbank3")
 DB_PASSWORD = os.environ.get('DB_PASSWORD', "massbank3password")
 DB_HOST = os.environ.get('DB_HOST', "localhost")
 DB_NAME = os.environ.get('DB_NAME', "massbank3")
 
+# Global variables for in-memory data
+timestamp = datetime.fromisoformat('2010-01-01')
+spectra = []
+
+# Initialize the connection pool globally
+pool = ConnectionPool(conninfo=f"postgresql://{DB_NAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", open=False)
+
+
+def load_spectra():  # noqa: E501
+    """load all spectra from the database if the metadata indicates newer data and stores them"""
+    global timestamp, spectra
+    pool.open()
+    with pool.connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM metadata;")
+            dbtimestamp = cur.fetchone()[2]
+        logging.info("Database timestamp: %s", dbtimestamp)
+        logging.info("In-memory timestamp: %s", timestamp)
+        timestamp_diff = dbtimestamp - timestamp
+        if timestamp_diff.total_seconds() > 0:
+            logging.info("Database timestamp is %s newer. Reloading...", timestamp_diff)
+
+            # Prevent matchms from complaining about spectra not having a precursor_mz
+            set_matchms_logger_level("ERROR")
+
+            with conn.cursor() as cur:
+                cur = conn.cursor()
+                cur.execute(
+                    "SELECT massbank.accession, peak.mz, peak.intensity "
+                    "FROM massbank "
+                    "JOIN spectrum ON massbank.id = spectrum.massbank_id "
+                    "JOIN peak ON spectrum.id = peak.spectrum_id "
+                    "ORDER BY massbank.id, peak.mz;"
+                )
+
+                entries = {}
+                spectra = []
+
+                for res in cur:
+                    accession, mz, intensity = res
+                    if accession not in entries:
+                        entries[accession] = {"mz": [], "intensity": []}
+                    entries[accession]["mz"].append(mz)
+                    entries[accession]["intensity"].append(intensity)
+
+                for accession, data in entries.items():
+                    spectra.append(
+                        normalize_intensities(Spectrum(mz=numpy.array(data["mz"]).astype(float),
+                                                       intensities=numpy.array(data["intensity"]).astype(float),
+                                                       metadata={'spectrum_id': accession})))
+
+                logging.info("Loaded %s spectra from the database.", len(spectra))
+                timestamp = dbtimestamp
 
 
 def similarity_post(similarity_calculation):  # noqa: E501
@@ -35,9 +91,7 @@ def similarity_post(similarity_calculation):  # noqa: E501
         request = SimilarityCalculation.from_dict(similarity_calculation)
 
         try:
-            reference_spectra = ReferenceSpectra(
-                psycopg.connect(f"postgresql://{DB_NAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"))
-            reference_spectra.load_spectra()
+            load_spectra()
         except psycopg.DatabaseError as e:
             return connexion.problem(
                 title="Database Error",
@@ -48,7 +102,7 @@ def similarity_post(similarity_calculation):  # noqa: E501
         mz, intensities = zip(*[(peak.mz, peak.intensity) for peak in request.peak_list])
 
         try:
-            query = normalize_intensities(Spectrum(mz=np.array(mz), intensities=np.array(intensities)))
+            query = normalize_intensities(Spectrum(mz=numpy.array(mz), intensities=numpy.array(intensities)))
         except AssertionError as e:
             return connexion.problem(
                 title="AssertionError",
@@ -56,7 +110,7 @@ def similarity_post(similarity_calculation):  # noqa: E501
                 status=400,
             )
 
-        references = ReferenceSpectra.spectra
+        references = spectra
         if request.reference_spectra_list:
             references = [s for s in references if s.metadata['spectrum_id'] in request.reference_spectra_list]
 
