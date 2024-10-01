7d68a26e54f8db45f8d54c4ee819c7f1fd93d3bd
Rene Meier <meier.rene@googlemail.com>
2024-10-01 10:50:15 +0200

    Remove similarity service, because it has its own repo now.

--- similarity-service-cosine/cosine_impl/__main__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-import cosine_impl.app
-
-
-if __name__ == '__main__':
-    cosine_impl.app.serve_app()
--- similarity-service-cosine/generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf gen
-java -jar openapi-generator-cli.jar generate -g python-flask -i openapi.yaml -o gen
