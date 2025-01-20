f2e8bc79e521753d44aeadb0b54101795ec48c95
michaelwenk <mwenk@ipb-halle.de>
2025-01-20 22:38:18 +0100

    test: vite@6 & allowedHosts

--- web-frontend/vite.config.ts
+++ web-frontend/vite.config.ts
@@ -4,6 +4,14 @@ import { defineConfig, splitVendorChunkPlugin, loadEnv } from 'vite';
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
 
+  const allowedHosts = [
+    '.' + 'msbi.ipb-halle.de' + process.env.VITE_MB3_BASE_URL,
+    // (process.env.VITE_MB3_FRONTEND_URL as string) +
+    // process.env.VITE_MB3_BASE_URL) as string,
+  ];
+
+  console.log('allowedHosts:', allowedHosts);
+
   return defineConfig({
     base: process.env.VITE_MB3_BASE_URL,
     css: {
@@ -27,5 +35,8 @@ export default ({ mode }) => {
       sourcemap: true,
     },
     plugins: [react(), splitVendorChunkPlugin()],
+    server: {
+      allowedHosts,
+    },
   });
 };
