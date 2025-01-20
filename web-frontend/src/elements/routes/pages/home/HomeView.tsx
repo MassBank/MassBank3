3030dc99bb6ffe9c97da10188d09d1f11ad11d74
michaelwenk <mwenk@ipb-halle.de>
2025-01-20 16:28:27 +0100

    test: google search console without react-helmet

--- web-frontend/src/elements/routes/pages/home/HomeView.tsx
+++ web-frontend/src/elements/routes/pages/home/HomeView.tsx
@@ -1,8 +1,7 @@
 import Layout, { Content, Header } from 'antd/es/layout/layout';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import AccessionSearchInputField from '../../../common/AccessionSearchInputField';
 import useContainerDimensions from '../../../../utils/useContainerDimensions';
-import { Helmet } from 'react-helmet';
 
 const accessionSearchInputFieldHeight = 50;
 
@@ -10,59 +9,55 @@ function HomeView() {
   const ref = useRef(null);
   const { height } = useContainerDimensions(ref);
 
+  useEffect(() => {
+    const googleMeta = document.createElement('meta');
+    googleMeta.name = 'google-site-verification';
+    googleMeta.content = '4aoZgYg2lHeh7TlOxtyVzjHa3YJirrsEHPqwSU3luoU';
+    document.head.appendChild(googleMeta);
+  }, []);
+
   return (
-    <>
-      <Helmet>
-        <title>MassBank Europe</title>
-        <meta
-          name="google-site-verification"
-          content="4aoZgYg2lHeh7TlOxtyVzjHa3YJirrsEHPqwSU3luoU"
-        />
-      </Helmet>
-      <Layout
-        ref={ref}
+    <Layout
+      ref={ref}
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Header
         style={{
           width: '100%',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
+          height: accessionSearchInputFieldHeight,
+          padding: 0,
         }}
       >
-        <Header
-          style={{
-            width: '100%',
-            height: accessionSearchInputFieldHeight,
-            padding: 0,
-          }}
-        >
-          <AccessionSearchInputField
-            width="100%"
-            height={accessionSearchInputFieldHeight}
-          />
-        </Header>
-        <Content
-          style={{
-            width: '100%',
-            height: height - accessionSearchInputFieldHeight,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <p style={{ fontWeight: 'bold', fontSize: 18 }}>
-            Welcome to MassBank!
-          </p>
-          <p style={{ textAlign: 'center' }}>
-            MassBank is a community effort and you are invited to contribute.
-            Please refer to our contributor documentation and get in touch via
-            github or email.
-          </p>
-        </Content>
-      </Layout>
-    </>
+        <AccessionSearchInputField
+          width="100%"
+          height={accessionSearchInputFieldHeight}
+        />
+      </Header>
+      <Content
+        style={{
+          width: '100%',
+          height: height - accessionSearchInputFieldHeight,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p style={{ fontWeight: 'bold', fontSize: 18 }}>Welcome to MassBank!</p>
+        <p style={{ textAlign: 'center' }}>
+          MassBank is a community effort and you are invited to contribute.
+          Please refer to our contributor documentation and get in touch via
+          github or email.
+        </p>
+      </Content>
+    </Layout>
   );
 }
 
