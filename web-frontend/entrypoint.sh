b4bf37bf9b7011e1051cfef2ac575aba3aa32d52
michaelwenk <mwenk@ipb-halle.de>
2025-01-21 18:35:25 +0100

    chore: use Google search control key via environment

--- web-frontend/entrypoint.sh
+++ web-frontend/entrypoint.sh
@@ -5,6 +5,7 @@ echo "VITE_MB3_FRONTEND_URL=$2" >> .env
 echo "VITE_MB3_BASE_URL=$3" >> .env
 echo "VITE_MB3_VERSION=$4" >> .env
 echo "VITE_EXPORT_SERVICE_URL=$5" >> .env
+echo "VITE_GOOGLE_SEARCH_CONSOLE_KEY=$6" >> .env
 
 
 npm run start
