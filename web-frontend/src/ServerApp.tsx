e8c7954c8b064f2666c498365e144049e68cb958
michaelwenk <mwenk@ipb-halle.de>
2025-02-09 22:13:50 +0100

    fix: removed StrictMode from ServerApp.tsx to avoid second render during production mode

--- web-frontend/src/ServerApp.tsx
+++ web-frontend/src/ServerApp.tsx
@@ -1,7 +1,6 @@
 import { renderToString } from 'react-dom/server';
 import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
 import { StaticRouter } from 'react-router-dom';
-import { StrictMode } from 'react';
 import { propertiesContext } from './context/properties/properties';
 import PropertiesContextProps from './types/PropertiesContextProps';
 import App from './App';
@@ -15,15 +14,13 @@ export const render = ({ path, props }: IRenderProps) => {
   // SSR Render
   const cache = createCache();
   const html = renderToString(
-    <StrictMode>
-      <StyleProvider cache={cache}>
-        <propertiesContext.Provider value={props ?? {}}>
-          <StaticRouter location={path}>
-            <App />
-          </StaticRouter>
-        </propertiesContext.Provider>
-      </StyleProvider>
-    </StrictMode>,
+    <StyleProvider cache={cache}>
+      <propertiesContext.Provider value={props ?? {}}>
+        <StaticRouter location={path}>
+          <App />
+        </StaticRouter>
+      </propertiesContext.Provider>
+    </StyleProvider>,
   );
 
   // Grab style from cache
