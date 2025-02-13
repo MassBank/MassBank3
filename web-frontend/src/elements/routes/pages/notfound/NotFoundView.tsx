d55fbc5cf63c3c6fea049b4bb1c6c8ef5ae3060f
Michael Wenk <mwenk@ipb-halle.de>
2025-02-13 12:51:31 +0100

    chore: added result component and go back to home option when page was not found

--- web-frontend/src/elements/routes/pages/notfound/NotFoundView.tsx
+++ web-frontend/src/elements/routes/pages/notfound/NotFoundView.tsx
@@ -1,18 +1,41 @@
+import { Button, Result } from 'antd';
 import { Content } from 'antd/es/layout/layout';
+import { useCallback, useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { usePropertiesContext } from '../../../../context/properties/properties';
 
 function NotFoundView() {
-  return (
-    <Content
-      style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <h2>404: Page Not Found</h2>
-    </Content>
+  const navigate = useNavigate();
+  const { baseUrl } = usePropertiesContext();
+
+  const handleOnNavigateHome = useCallback(() => {
+    navigate(baseUrl);
+  }, [baseUrl, navigate]);
+
+  return useMemo(
+    () => (
+      <Content
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Result
+          status="404"
+          title="404"
+          subTitle="Sorry, the page you visited does not exist."
+          extra={
+            <Button type="primary" onClick={handleOnNavigateHome}>
+              Back Home
+            </Button>
+          }
+        />
+      </Content>
+    ),
+    [handleOnNavigateHome],
   );
 }
 
