c6989651b1dabd311cb303dccd294d3ec8678e8e
Michael Wenk <mwenk@ipb-halle.de>
2025-06-05 17:06:35 +0200

    fix: set url field with complete url in openapi.json

--- entrypoint-mb3server.sh
+++ entrypoint-mb3server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo -n "servers:\n  - url: \"" >> swagger-ui/openapi.yaml
+echo -e -n 'servers:\n - url: "' >> swagger-ui/openapi.yaml
 echo -n $1 >> swagger-ui/openapi.yaml
 echo -n $2 >> swagger-ui/openapi.yaml
 echo "\"" >> swagger-ui/openapi.yaml
+
+swagger-cli bundle swagger-ui/openapi.yaml -o swagger-ui/openapi.json -t json
 
 CGO_ENABLED=0
 go get -d -v ./...
