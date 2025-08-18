24f1f57318d1630f18e4f93697792fb6a3566f24
Michael Wenk <mwenk@ipb-halle.de>
2025-08-18 16:51:03 +0200

    chore: merge dev branch into main branch when executing release.sh

--- release.sh
+++ release.sh
@@ -3,5 +3,10 @@
 version=$(head -n 1 version.txt)
 git commit --allow-empty -m "chore: release $version" -m "release-as: $version"
 
-# Push the changes to the remote repository
+# Push the changes to the main branch (remote)
+git checkout main 
+git merge dev
 git push origin main
+
+# Switch back to the development branch
+git checkout dev
